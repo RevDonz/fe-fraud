efce90fb5706e1f28114176553e3ebef4b94ed7d
RevDonz <aprilio842@gmail.com>
2024-05-15 22:00:01 +0700

    fix: fix 404 page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,3 +1,20 @@
+import { Button } from "@nextui-org/react";
+import Image from "next/image";
+import Link from "next/link";
+
 export default function NotFound() {
-	return <div>NotFound</div>;
+	return (
+		<div className="flex items-center justify-center min-h-screen h-full w-full flex-col gap-5">
+			<Image
+				src={"/assets/img/404.svg"}
+				alt="404 NOt Found"
+				height={400}
+				width={400}
+			/>
+			<p>Halaman tidak dapat ditemukan!</p>
+			<Link href={"/dashboard"}>
+				<Button color="primary">Kembali ke Halaman Utama</Button>
+			</Link>
+		</div>
+	);
 }
