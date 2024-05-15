ee48a9d0d248e898eb0eb8423062df2935e2079a
RevDonz <aprilio842@gmail.com>
2024-05-15 21:52:39 +0700

    fix: fix middleware

--- src/middleware.tsx
+++ src/middleware.tsx
@@ -1,63 +1,32 @@
 import { getToken } from "next-auth/jwt";
-import type { NextMiddleware } from "next/server";
+import type { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
 import { Routes } from "./constant/routes";
 
-export const middleware: NextMiddleware = async (request) => {
-	const pathname = request.nextUrl.pathname;
+export async function middleware(req: NextRequest) {
+	const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
+	const { pathname } = req.nextUrl;
 
-	const token = await getToken({
-		req: request,
-		secret: process.env.NEXTAUTH_SECRET,
-	});
-
-	// try {
-	// 	const response = await fetch(
-	// 		`${process.env.NEXT_PUBLIC_BASE_URL}/api/auth`,
-	// 		{
-	// 			headers: { Authorization: `Bearer ${token?.id}` },
-	// 		},
-	// 	);
-
-	// 	const res = await response.json();
-
-	// 	if (!token && !["/auth/login", "/auth/register", "/"].includes(pathname)) {
-	// 		return NextResponse.redirect(new URL("/auth/login", request.url));
-	// 	}
-
-	// 	const firstPath = `/${pathname.split("/")[1]}/${pathname.split("/")[2]}`;
-
-	// 	const { success, code, data } = res;
-	// 	if (success && code === 200) {
-	// 		const isAuthorize = checkUserPermission(pathname, data.role);
-
-	// 		if (
-	// 			!isAuthorize &&
-	// 			!["/not-found", "/auth/login", "/root/login", "/"].includes(pathname)
-	// 		) {
-	// 			return NextResponse.redirect(new URL("/not-found", request.url));
-	// 		}
-
-	// 		switch (pathname) {
-	// 			case "/":
-	// 				return NextResponse.redirect(new URL("/dashboard", request.url));
-	// 		}
-	// 	}
-	// } catch (error) {
-	// 	console.error("Error during API call:", error);
-
-	// 	return NextResponse.redirect(new URL("/error", request.url));
-	// }
-};
-
-const checkUserPermission = (firstPath: string, userRole: string) => {
-	const route = Routes.find((route) => route.url === firstPath);
-
-	if (!route) {
-		return false;
+	if (token && (pathname === "/auth/login" || pathname === "/auth/register")) {
+		return NextResponse.redirect(new URL("/dashboard", req.url));
 	}
 
-	return route.role.includes(userRole);
-};
+	const route = Routes.find((route) => pathname.startsWith(route.url));
+
+	if (route) {
+		if (!token) {
+			return NextResponse.redirect(new URL("/auth/login", req.url));
+		}
+
+		const userHasAccess = route.role.includes(token.role);
+
+		if (!userHasAccess) {
+			return NextResponse.redirect(new URL("/403", req.url));
+		}
+	}
+
+	return NextResponse.next();
+}
 
 export const config = {
 	matcher: ["/((?!api|_next/static|_next/image|favicon.ico|assets).*)"],
