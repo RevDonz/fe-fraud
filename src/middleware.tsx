c27871642523e1591c21439eeaf469fc93ef1c1c
RevDonz <aprilio842@gmail.com>
2024-05-16 19:50:40 +0700

    tes middleware

--- src/middleware.tsx
+++ src/middleware.tsx
@@ -1,33 +1,33 @@
-import { getToken } from "next-auth/jwt";
-import type { NextRequest } from "next/server";
-import { NextResponse } from "next/server";
-import { Routes } from "./constant/routes";
+// import { getToken } from "next-auth/jwt";
+// import type { NextRequest } from "next/server";
+// import { NextResponse } from "next/server";
+// import { Routes } from "./constant/routes";
 
-export async function middleware(req: NextRequest) {
-	const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
-	const { pathname } = req.nextUrl;
+// export async function middleware(req: NextRequest) {
+// 	const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
+// 	const { pathname } = req.nextUrl;
 
-	if (token && (pathname === "/auth/login" || pathname === "/auth/register")) {
-		return NextResponse.redirect(new URL("/dashboard", req.url));
-	}
+// 	if (token && (pathname === "/auth/login" || pathname === "/auth/register")) {
+// 		return NextResponse.redirect(new URL("/dashboard", req.url));
+// 	}
 
-	const route = Routes.find((route) => pathname.startsWith(route.url));
+// 	const route = Routes.find((route) => pathname.startsWith(route.url));
 
-	if (route) {
-		if (!token) {
-			return NextResponse.redirect(new URL("/auth/login", req.url));
-		}
+// 	if (route) {
+// 		if (!token) {
+// 			return NextResponse.redirect(new URL("/auth/login", req.url));
+// 		}
 
-		const userHasAccess = route.role.includes(token.role);
+// 		const userHasAccess = route.role.includes(token.role);
 
-		if (!userHasAccess) {
-			return NextResponse.redirect(new URL("/404", req.url));
-		}
-	}
+// 		if (!userHasAccess) {
+// 			return NextResponse.redirect(new URL("/404", req.url));
+// 		}
+// 	}
 
-	return NextResponse.next();
-}
+// 	return NextResponse.next();
+// }
 
-export const config = {
-	matcher: ["/((?!api|_next/static|_next/image|favicon.ico|assets).*)"],
-};
+// export const config = {
+// 	matcher: ["/((?!api|_next/static|_next/image|favicon.ico|assets).*)"],
+// };
