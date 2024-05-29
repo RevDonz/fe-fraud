96e0c4ec21ce71b12c733e90427dba833d3bd642
RevDonz <aprilio842@gmail.com>
2024-05-29 20:03:23 +0700

    chore: change create assesment to client side

--- src/app/dashboard/fraud-assesment/create/page.tsx
+++ src/app/dashboard/fraud-assesment/create/page.tsx
@@ -1,23 +1,9 @@
 import ButtonLink from "@/components/button-link-assesment";
 import ModalAssesment from "@/components/modal-fraud-assesment";
-import { ListSubBab, Questions } from "@/constant/assesment";
-import {
-	checkAssesment,
-	getAssesmentHistory,
-	getFinishedAssesment,
-} from "@/lib/assesment";
+import { checkAssesment, getAssesmentHistory } from "@/lib/assesment";
 import { getServerAuthSession } from "@/lib/auth";
-import {
-	Button,
-	Card,
-	CardBody,
-	CardHeader,
-	Divider,
-	Tooltip,
-} from "@nextui-org/react";
-import { Check } from "lucide-react";
 import type { Metadata } from "next";
-import Link from "next/link";
+import QuestionsList from "./questions";
 import SubmitButton from "./submit-button";
 
 export const metadata: Metadata = {
@@ -28,15 +14,8 @@ export const metadata: Metadata = {
 
 export default async function FillAssesmentPage() {
 	const session = await getServerAuthSession();
-	const token = session?.user.accessToken;
+	const token = session?.user.accessToken as string;
 	const fraudHistory = await getAssesmentHistory(token as string);
-
-	const finished: string[] = await getFinishedAssesment(token as string);
-
-	const unFinished =
-		finished.length > 0
-			? ListSubBab.filter((item) => !finished.includes(item.toString()))
-			: [];
 
 	const isAttempAssesment = await checkAssesment(token as string);
 
@@ -48,109 +27,10 @@ export default async function FillAssesmentPage() {
 			<ButtonLink />
 
 			<div className="flex flex-col gap-5">
-				{/* <ModalAssesment /> */}
-				{Questions.map((question, index) => {
-					return (
-						<Card key={`${index * 2}`}>
-							<CardHeader>
-								<p className="font-semibold">
-									{index + 1}. {question.title}
-								</p>
-							</CardHeader>
-							{question.subtitle.map((subquestion, subIndex) => {
-								return (
-									<div key={`${subIndex * 2}`}>
-										<Divider />
-										<CardBody>
-											<div className="flex items-center justify-between ml-4">
-												<p>
-													{index + 1}.{subIndex + 1}. {subquestion.title}
-												</p>
-
-												{finished.length > 0 ? (
-													finished.includes(subquestion.sub_bab.toString()) ? (
-														<div className="flex gap-3" key={`${index * 2}`}>
-															<Button
-																size="sm"
-																color="warning"
-																className="text-white"
-																href={`/dashboard/fraud-assesment/edit/${
-																	index + 1
-																}/${index + 1}.${subIndex + 1}`}
-																as={Link}
-															>
-																Edit
-															</Button>
-															<Button
-																color="success"
-																isIconOnly
-																size="sm"
-																className="text-white"
-															>
-																<Check className="w-4 h-4" />
-															</Button>
-														</div>
-													) : unFinished[0] === subquestion.sub_bab ? (
-														<Button
-															size="sm"
-															color="primary"
-															href={`/dashboard/fraud-assesment/create/${
-																index + 1
-															}/${index + 1}.${subIndex + 1}`}
-															as={Link}
-														>
-															Mulai
-														</Button>
-													) : (
-														<Tooltip
-															content="Selesaikan assesment sebelumnya!"
-															color="primary"
-															placement="left"
-															showArrow
-														>
-															<div className="">
-																<Button size="sm" color="primary" isDisabled>
-																	Mulai
-																</Button>
-															</div>
-														</Tooltip>
-													)
-												) : index === 0 && subIndex === 0 ? (
-													<Button
-														size="sm"
-														color="primary"
-														href={`/dashboard/fraud-assesment/create/${
-															index + 1
-														}/${index + 1}.${subIndex + 1}`}
-														as={Link}
-													>
-														Mulai
-													</Button>
-												) : (
-													<Tooltip
-														content="Selesaikan assesment sebelumnya!"
-														color="primary"
-														placement="left"
-														showArrow
-													>
-														<div className="">
-															<Button size="sm" color="primary" isDisabled>
-																Mulai
-															</Button>
-														</div>
-													</Tooltip>
-												)}
-											</div>
-										</CardBody>
-									</div>
-								);
-							})}
-						</Card>
-					);
-				})}
+				<QuestionsList token={token} />
 				<SubmitButton
 					id={isAttempAssesment && assesment[0].key}
-					token={token as string}
+					token={token}
 				/>
 			</div>
 		</>
