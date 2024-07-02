072474d5eba98d767fa93856612d9bb41b27b272
RevDonz <aprilio842@gmail.com>
2024-07-02 22:40:49 +0700

    feat: add submit fraud detection

--- src/app/dashboard/fraud-detection/create/form.tsx
+++ src/app/dashboard/fraud-detection/create/form.tsx
@@ -1,8 +1,16 @@
 "use client";
+import { DataTableRow, FinancialRatiosIndexes } from "@/constant/detection";
 import { getEntity } from "@/lib/entity";
-import { Divider } from "@nextui-org/react";
-import { useQuery } from "@tanstack/react-query";
+import { detectionSchema } from "@/schema/fraud/detection";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Button, Checkbox, Divider } from "@nextui-org/react";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 import CurrencyInput from "react-currency-input-field";
+import { Controller, useForm } from "react-hook-form";
+import { toast } from "sonner";
+import type { z } from "zod";
 
 export default function FormDetection({ token }: { token: string }) {
 	const { data, isPending } = useQuery({
@@ -12,75 +20,122 @@ export default function FormDetection({ token }: { token: string }) {
 			return data;
 		},
 	});
+	const [isSelected, setIsSelected] = useState(false);
+	const router = useRouter();
 
-	const DataTableRow = [
-		{
-			variable: "Revenue",
+	const {
+		handleSubmit,
+		control,
+		formState: { errors },
+	} = useForm<z.infer<typeof detectionSchema>>({
+		resolver: zodResolver(detectionSchema),
+		// defaultValues: {
+		// 	revenue_1: undefined,
+		// 	cogs_1: undefined,
+		// 	sgae_1: undefined,
+		// 	depreciation_1: undefined,
+		// 	net_continuous_1: undefined,
+		// 	account_receivables_1: undefined,
+		// 	current_assets_1: undefined,
+		// 	ppe_1: undefined,
+		// 	securities_1: undefined,
+		// 	total_asset_1: undefined,
+		// 	current_liabilities_1: undefined,
+		// 	total_ltd_1: undefined,
+		// 	cash_flow_operate_1: undefined,
+		// 	revenue_2: undefined,
+		// 	cogs_2: undefined,
+		// 	sgae_2: undefined,
+		// 	depreciation_2: undefined,
+		// 	net_continuous_2: undefined,
+		// 	account_receivables_2: undefined,
+		// 	current_assets_2: undefined,
+		// 	ppe_2: undefined,
+		// 	securities_2: undefined,
+		// 	total_asset_2: undefined,
+		// 	current_liabilities_2: undefined,
+		// 	total_ltd_2: undefined,
+		// 	cash_flow_operate_2: undefined,
+		// 	tahun_1: 2012,
+		// 	tahun_2: 2013,
+		// 	id_institution: "gc8uupscjs0e",
+		// },
+		defaultValues: {
+			revenue_1: 2824529865,
+			cogs_1: 1637778233,
+			sgae_1: 978404721,
+			depreciation_1: 145556996,
+			net_continuous_1: 48164343,
+			account_receivables_1: 153909929,
+			current_assets_1: 955934379,
+			ppe_1: 1520837177,
+			securities_1: 11703633,
+			total_asset_1: 2085215,
+			current_liabilities_1: 919187828,
+			total_ltd_1: 602268672,
+			cash_flow_operate_1: 292733763,
+			revenue_2: 3698268848,
+			cogs_2: 2017661985,
+			sgae_2: 1286510421,
+			depreciation_2: 199692257,
+			net_continuous_2: 159960247,
+			account_receivables_2: 211364131,
+			current_assets_2: 1192611390,
+			ppe_2: 1520837177,
+			securities_2: 14959054,
+			total_asset_2: 2582866504,
+			current_liabilities_2: 1086055919,
+			total_ltd_2: 631821538,
+			cash_flow_operate_2: 487884312,
+			tahun_1: 2012,
+			tahun_2: 2013,
+			id_institution: "gc8uupscjs0e",
 		},
-		{
-			variable: "Cost of Goods Sold",
-		},
-		{
-			variable: "Selling, General, & Admin. Expense",
-		},
-		{
-			variable: "Depreciation",
-		},
-		{
-			variable: "Net Income from Continuing Operations",
-		},
-		{
-			variable: "Accounts Receivables",
-		},
-		{
-			variable: "Current Assets",
-		},
-		{
-			variable: "Property, Plants, & Equipment",
-		},
-		{
-			variable: "Securities",
-		},
-		{
-			variable: "Total Assets",
-		},
-		{
-			variable: "Current Liabilities",
-		},
-		{
-			variable: "Total Long-term Debt",
-		},
-		{
-			variable: "Cash Flow from Operations",
-		},
-	];
+	});
 
-	const FinancialRatiosIndexes = [
-		{
-			index: "Day Sales in Receivables Index (DSRI)",
+	type FraudDetectionType = z.infer<typeof detectionSchema>;
+
+	const mutation = useMutation({
+		mutationKey: ["submit-assesment"],
+		mutationFn: async (values: z.infer<typeof detectionSchema>) => {
+			const response = await fetch(
+				`${process.env.NEXT_PUBLIC_BASE_URL}/report`,
+				{
+					method: "POST",
+					body: JSON.stringify(values),
+					headers: {
+						Authorization: `Bearer ${token}`,
+						"Content-Type": "application/json",
+					},
+				},
+			);
+
+			if (!response.ok) {
+				throw new Error("Failed to submit data");
+			}
+
+			const result = await response.json();
+			return result;
 		},
-		{
-			index: "Gross Margin Index (GMI)",
+
+		onMutate() {
+			toast.loading("Loading...");
 		},
-		{
-			index: "Asset Quality Index (AQI)",
+		onSuccess(data) {
+			toast.dismiss();
+			toast.success("Berhasil");
+			console.log(data);
 		},
-		{
-			index: "Sales Growth Index (SGI)",
+		onError(error) {
+			toast.dismiss();
+			toast.error("Gagal submit assesment!");
+			console.log("Error submit", error.message);
 		},
-		{
-			index: "Depreciation Index (DEPI)",
-		},
-		{
-			index: "Selling, General, & Admin. Expenses Index (SGAI)",
-		},
-		{
-			index: "Leverage Index (LVGI)",
-		},
-		{
-			index: "Total Accruals to Total Assets (TATA)",
-		},
-	];
+	});
+
+	const onSubmit = async (values: z.infer<typeof detectionSchema>) => {
+		mutation.mutate(values);
+	};
 
 	return (
 		<div>
@@ -121,78 +176,126 @@ export default function FormDetection({ token }: { token: string }) {
 			</div>
 			<Divider />
 
-			<div className="py-5">
-				<div className="grid grid-cols-3 border rounded-tl-lg rounded-tr-lg">
-					<div className="bg-default-100 px-3 py-2 text-sm font-medium text-foreground-500 rounded-tl-lg">
-						( IN MILLION )
-					</div>
-					<div className="bg-default-100 px-3 py-2 text-sm font-medium text-foreground-500">
-						YEAR 2 (Rupiah)
-					</div>
-					<div className="bg-default-100 px-3 py-2 text-sm font-medium text-foreground-500 rounded-tr-lg">
-						YEAR 1 (Rupiah)
-					</div>
-				</div>
-				{DataTableRow.map((row) => {
-					return (
-						<div
-							className="grid grid-cols-3 gap-1 border-x border-b"
-							key={row.variable}
-						>
-							<div className="px-3 py-2 border-r">{row.variable}</div>
-							<div className="border-r">
-                <CurrencyInput className="px-3 py-2 w-full focus:outline-none text-right" />
-							</div>
-							<div className="">
-                <CurrencyInput className="px-3 py-2 w-full focus:outline-none text-right" />
-							</div>
+			<form onSubmit={handleSubmit(onSubmit)}>
+				<div className="py-5">
+					<div className="grid grid-cols-3 border rounded-tl-lg rounded-tr-lg">
+						<div className="bg-default-100 px-3 py-2 text-sm font-medium text-foreground-500 rounded-tl-lg">
+							( IN MILLION )
 						</div>
-					);
-				})}
-				<p className="italic mt-3 ml-3">
-					Securities is also referred to as total long term investments
-				</p>
-			</div>
-			<div className="py-5">
-				<div className="grid grid-cols-4 border rounded-tl-lg rounded-tr-lg">
-					<div className="bg-default-100 px-3 py-2 text-sm font-medium text-foreground-500 rounded-tl-lg">
-						Financial Ratios Indexes
-					</div>
-					<div className="bg-default-100 px-3 py-2 text-sm font-medium text-foreground-500">
-						YEAR 2
-					</div>
-					<div className="bg-default-100 px-3 py-2 text-sm font-medium text-foreground-500">
-						YEAR 1
-					</div>
-					<div className="bg-default-100 px-3 py-2 text-sm font-medium text-foreground-500 rounded-tr-lg">
-						INDEX
-					</div>
-				</div>
-				{FinancialRatiosIndexes.map((row) => {
-					return (
-						<div
-							className="grid grid-cols-4 gap-1 border-x border-b"
-							key={row.index}
-						>
-							<div className="px-3 py-2 border-r">{row.index}</div>
-							<div className="border-r">
-								<input type="number" className="w-full focus:outline-none" />
-							</div>
-							<div className="border-r">
-								<input type="number" className="w-full focus:outline-none" />
-							</div>
+						<div className="bg-default-100 px-3 py-2 text-sm font-medium text-foreground-500">
+							YEAR 2 (Rupiah)
 						</div>
-					);
-				})}
-				<div className="grid grid-cols-4 border-b border-x rounded-bl-lg rounded-br-lg">
-					<div className="bg-default-100 px-3 py-2 text-sm font-medium text-foreground-700 rounded-bl-lg col-span-3">
-						Beneish M Score
+						<div className="bg-default-100 px-3 py-2 text-sm font-medium text-foreground-500 rounded-tr-lg">
+							YEAR 1 (Rupiah)
+						</div>
 					</div>
-					<div className="bg-green-500 px-3 py-2 text-sm font-medium text-foreground-100 rounded-br-lg">
-						-2.00
+					{DataTableRow.map((row) => {
+						return (
+							<div
+								className="grid grid-cols-3 gap-1 border-x border-b"
+								key={row.name}
+							>
+								<div className="px-3 py-2 border-r">{row.label}</div>
+								<div className="border-r">
+									<Controller
+										name={`${row.name}_1` as keyof FraudDetectionType}
+										control={control}
+										render={({ field }) => (
+											<CurrencyInput
+												className="px-3 py-2 w-full focus:outline-none text-right"
+												value={field.value}
+												onValueChange={(value) => {
+													field.onChange(Number(value));
+												}}
+											/>
+										)}
+									/>
+								</div>
+								<div className="">
+									<Controller
+										name={`${row.name}_2` as keyof FraudDetectionType}
+										control={control}
+										render={({ field }) => (
+											<CurrencyInput
+												className="px-3 py-2 w-full focus:outline-none text-right"
+												value={field.value}
+												onValueChange={(value) => {
+													field.onChange(Number(value));
+												}}
+											/>
+										)}
+									/>
+								</div>
+							</div>
+						);
+					})}
+					<p className="italic mt-3 ml-3">
+						Securities is also referred to as total long term investments
+					</p>
+				</div>
+				<div className="py-5">
+					<div className="grid grid-cols-4 border rounded-tl-lg rounded-tr-lg">
+						<div className="bg-default-100 px-3 py-2 text-sm font-medium text-foreground-500 rounded-tl-lg">
+							Financial Ratios Indexes
+						</div>
+						<div className="bg-default-100 px-3 py-2 text-sm font-medium text-foreground-500">
+							YEAR 2
+						</div>
+						<div className="bg-default-100 px-3 py-2 text-sm font-medium text-foreground-500">
+							YEAR 1
+						</div>
+						<div className="bg-default-100 px-3 py-2 text-sm font-medium text-foreground-500 rounded-tr-lg">
+							INDEX
+						</div>
+					</div>
+					{FinancialRatiosIndexes.map((row) => {
+						return (
+							<div
+								className="grid grid-cols-4 gap-1 border-x border-b"
+								key={row.index}
+							>
+								<div className="px-3 py-2 border-r">{row.index}</div>
+								<div className="border-r">
+									<input type="number" className="w-full focus:outline-none" />
+								</div>
+								<div className="border-r">
+									<input type="number" className="w-full focus:outline-none" />
+								</div>
+							</div>
+						);
+					})}
+					<div className="grid grid-cols-4 border-b border-x rounded-bl-lg rounded-br-lg">
+						<div className="bg-default-100 px-3 py-2 text-sm font-medium text-foreground-700 rounded-bl-lg col-span-3">
+							Beneish M Score
+						</div>
+						<div className="bg-green-500 px-3 py-2 text-sm font-medium text-foreground-100 rounded-br-lg">
+							-2.00
+						</div>
 					</div>
 				</div>
-			</div>
+				<div className="flex flex-col mt-5 gap-5">
+					<p className="text-danger text-justify">
+						Dengan ini menyatakan bahwa saya telah mengisi assesment ini dengan
+						itikad baik dan sepenuh hati. Saya berkomitmen untuk memberikan
+						jawaban yang sejelas dan seakurat mungkin, tanpa menggunakan bantuan
+						pihak lain atau materi yang tidak diizinkan. Kejujuran saya dalam
+						mengisi laporan ini adalah landasan untuk pengembangan diri saya.
+					</p>
+					<div className="flex flex-row items-center justify-between">
+						<Checkbox isSelected={isSelected} onValueChange={setIsSelected}>
+							Saya setuju dengan pernyataan diatas
+						</Checkbox>
+						<Button
+							color="success"
+							className="text-white"
+							isDisabled={!isSelected}
+							type="submit"
+						>
+							Submit
+						</Button>
+					</div>
+				</div>
+			</form>
 		</div>
 	);
 }
---
+++ src/constant/detection.ts
@@ -0,0 +1,45 @@
+export const DataTableRow = [
+	{ label: "Revenue", name: "revenue" },
+	{ label: "Cost of Goods Sold", name: "cogs" },
+	{ label: "Selling, General, & Admin. Expense", name: "sgae" },
+	{ label: "Depreciation", name: "depreciation" },
+	{
+		label: "Net Income from Continuing Operations",
+		name: "net_continuous",
+	},
+	{ label: "Accounts Receivables", name: "account_receivables" },
+	{ label: "Current Assets", name: "current_assets" },
+	{ label: "Property, Plants, & Equipment", name: "ppe" },
+	{ label: "Securities", name: "securities" },
+	{ label: "Total Assets", name: "total_asset" },
+	{ label: "Current Liabilities", name: "current_liabilities" },
+	{ label: "Total Long-term Debt", name: "total_ltd" },
+	{ label: "Cash Flow from Operations", name: "cash_flow_operate" },
+];
+
+export const FinancialRatiosIndexes = [
+	{
+		index: "Day Sales in Receivables Index (DSRI)",
+	},
+	{
+		index: "Gross Margin Index (GMI)",
+	},
+	{
+		index: "Asset Quality Index (AQI)",
+	},
+	{
+		index: "Sales Growth Index (SGI)",
+	},
+	{
+		index: "Depreciation Index (DEPI)",
+	},
+	{
+		index: "Selling, General, & Admin. Expenses Index (SGAI)",
+	},
+	{
+		index: "Leverage Index (LVGI)",
+	},
+	{
+		index: "Total Accruals to Total Assets (TATA)",
+	},
+];
